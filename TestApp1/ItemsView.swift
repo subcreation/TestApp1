8daf463bc3cff0a3ef914f836220d9e346e8eaca
subcreation <in8media@gmail.com>
2024-06-10 21:14:26 -0700

    First checkin

---
+++ TestApp1/ItemsView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  TestApp1
+//
+//  Created by Nathanael Roberton on 6/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ItemsView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    NavigationLink {
+                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                    } label: {
+                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+#if os(macOS)
+            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
+#endif
+            .toolbar {
+#if os(iOS)
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+#endif
+                ToolbarItem {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+            
+            Text("Select an item")
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+#Preview {
+    ItemsView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
---
+++ TestApp1/SidebarView.swift
@@ -0,0 +1,56 @@
+//
+//  SidebarView.swift
+//  TestApp1
+//
+//  Created by Nathanael Roberton on 6/10/24.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        #if os(macOS)
+        NavigationView {
+            List {
+                NavigationLink(destination: ItemsView()) {
+                    Label("Items", systemImage: "checklist")
+                }
+                NavigationLink(destination: Text("Social")) {
+                    Label("Social", systemImage: "person.3")
+                }
+                NavigationLink(destination: Text("Settings")) {
+                    Label("Settings", systemImage: "gear")
+                }
+            }
+            
+            ItemsView()
+        }
+        .listStyle(.sidebar)
+        .toolbar {
+            ToolbarItem {
+                // Hack to maintain header height across navigation
+                Button(action: {}) {
+                    Label("Add Item", systemImage: "plus")
+                }
+                .opacity(0.0)
+            }
+        }
+        #else
+        TabView {
+            Tab("Items", image: "pencil.and.list.clipboard") {
+                ItemsView()
+            }
+            Tab("Social", image: "person.3") {
+                Text("Social")
+            }
+            Tab("Settings", image: "gear") {
+                Text("Settings")
+            }
+        }
+        #endif
+    }
+}
+
+#Preview {
+    SidebarView()
+}
